85ccc6a731e5dd22e41fc9b5709a9991c1c37cd3
Anders Birkenes <datagutten@datagutten.net>
2025-07-05 14:36:46 +0200

    Return None when registration number is not found

--- pyvegvesen/kjoretoydata.py
+++ pyvegvesen/kjoretoydata.py
@@ -1,16 +1,19 @@
 import os
 import sys
+from typing import Optional
 
 import requests
 
 from .model import Model
 
 
-def kjoretoydata(kjennemerke: str) -> Model:
+def kjoretoydata(kjennemerke: str) -> Optional[Model]:
     url = 'https://akfell-datautlevering.atlas.vegvesen.no/enkeltoppslag/kjoretoydata'
     response = requests.get(url, {'kjennemerke': kjennemerke}, headers={
         'SVV-Authorization': os.environ.get('VEGVESEN_API_KEY'),
     })
+    if response.status_code == 204:
+        return None
     response.raise_for_status()
     return Model(**response.json())
 
